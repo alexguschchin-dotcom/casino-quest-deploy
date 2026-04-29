33c2c1d96ada53839d3dea1e34f41a46e44291b3
alexguschchin-dotcom <alexguschchin@gmail.com>
2026-04-29 21:12:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+// Где искать index.html?
+let staticDir = __dirname;
+const searchPaths = [
+    __dirname,                      // корень репозитория
+    path.join(__dirname, 'public'), // папка public
+    path.join(__dirname, 'src')     // папка src (вдруг)
+];
+for (const p of searchPaths) {
+    if (fs.existsSync(path.join(p, 'index.html'))) {
+        staticDir = p;
+        break;
+    }
+}
 
-app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
+console.log(`✅ Статика из папки: ${staticDir}`);
+app.use(express.static(staticDir));
+
+// Все запросы → index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(staticDir, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
