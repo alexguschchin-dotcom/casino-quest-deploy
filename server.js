9b4036832712065f325f8ea1333d9103ca7f490f
alexguschchin-dotcom <alexguschchin@gmail.com>
2026-04-29 21:08:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Раздаём статику из папки, где лежит server.js
 app.use(express.static(__dirname));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-// Все запросы -> index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
